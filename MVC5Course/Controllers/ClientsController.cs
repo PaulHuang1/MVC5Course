0bdd96311c0fe4e56ae3b42220d7f945305b8ad6
PaulHuang <paul780524@hotmail.com>
2016-10-15 15:40:49 +0800

    14 修改 /Clients/Index 頁面，加上 FirstName 的搜尋功能

--- MVC5Course/Controllers/ClientsController.cs
+++ MVC5Course/Controllers/ClientsController.cs
@@ -15,10 +15,17 @@ namespace MVC5Course.Controllers
         private FabricsEntities db = new FabricsEntities();
 
         // GET: Clients
-        public ActionResult Index()
+        public ActionResult Index(string search)
         {
             var client = db.Client.Include(c => c.Occupation);
+
+            if(string.IsNullOrEmpty(search))
+            {
+                client = client.Where(c => c.FirstName.Contains(search));
+            }
+
             client = client.OrderByDescending(c=>c.ClientId).Take(10);
+
             return View(client.ToList());
         }
 
@@ -41,7 +48,11 @@ namespace MVC5Course.Controllers
         public ActionResult Create()
         {
             ViewBag.OccupationId = new SelectList(db.Occupation, "OccupationId", "OccupationName");
-            return View();
+            var client = new Client
+            {
+                Gender = "M"
+            };
+            return View(client);
         }
 
         // POST: Clients/Create
