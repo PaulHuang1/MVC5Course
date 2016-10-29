f6b5a1cd9e82445e0245da203abfba6c51c68377
PaulHuang <paul780524@hotmail.com>
2016-10-29 15:24:04 +0800

    43 練習複雜模型繫結並可一次傳入多筆複雜模型資料繫結到陣列裡

--- MVC5Course/Controllers/MBController.cs
+++ MVC5Course/Controllers/MBController.cs
@@ -7,7 +7,7 @@ using System.Web.Mvc;
 
 namespace MVC5Course.Controllers
 {
-    public class MBController : Controller
+    public class MBController : BaseController
     {
         // GET: MB
         public ActionResult Index()
@@ -46,6 +46,40 @@ namespace MVC5Course.Controllers
 
         public ActionResult MyFormResult()
         {
+            return View();
+        }
+
+        public ActionResult ProductList()
+        {
+            var data = db.Product.OrderBy(p => p.ProductId).Take(10).ToList();
+
+            return View(data);
+        }
+
+        public ActionResult BatchUpdate(ProductBatchUpdateViewModel[] items)
+        {
+            /*
+             *              item.ProductId
+             *              item[0].ProductId
+             *              item[1].ProductId
+             */
+
+            if (ModelState.IsValid)
+            {
+                foreach (var item in items)
+                {
+                    var product = db.Product.Find(item.ProductId);
+                    product.ProductName = item.ProductName;
+                    product.Active = item.Active;
+                    product.Price = item.Price;
+                    product.Stock = item.Stock;
+                }
+
+                db.SaveChanges();
+
+                return RedirectToAction("ProductList");
+            }
+             
             return View();
         }
     }
