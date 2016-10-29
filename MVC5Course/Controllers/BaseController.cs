424859e023db6b72a1814bbe3fbed46036d0756b
PaulHuang <paul780524@hotmail.com>
2016-10-29 11:48:29 +0800

    38 練習 BaseController 設定與練習 HandleUnknownAction 的用法

---
+++ MVC5Course/Controllers/BaseController.cs
@@ -0,0 +1,20 @@
+﻿using MVC5Course.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC5Course.Controllers
+{
+    public abstract class BaseController : Controller
+    {
+        protected FabricsEntities db = new FabricsEntities();
+        //可用於找不到Controller的Action的時機
+        protected override void HandleUnknownAction(string actionName)
+        {
+            //base.HandleUnknownAction(actionName);
+            this.RedirectToAction("Index").ExecuteResult(this.ControllerContext);
+        }
+    }
+}
