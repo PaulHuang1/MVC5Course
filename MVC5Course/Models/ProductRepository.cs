fa21494259a5d6acd59588b071938e07f6d92ac4
PaulHuang <paul780524@hotmail.com>
2016-10-29 10:25:55 +0800

    36 練習用 ProductRepository 整理更抽象的資料存取方式

--- MVC5Course/Models/ProductRepository.cs
+++ MVC5Course/Models/ProductRepository.cs
@@ -6,9 +6,19 @@ namespace MVC5Course.Models
 {   
 	public  class ProductRepository : EFRepository<Product>, IProductRepository
 	{
+
+        public override IQueryable<Product> All()
+        {
+            return base.All().Where(p=>p.IsDeleted == false);
+        }
         public Product Find(int id)
         {
             return this.All().FirstOrDefault(p => p.ProductId == id);
+        }
+
+        public IQueryable<Product> Get所有資料_依據ProductId排序(int takeSize)
+        {
+            return this.All().OrderByDescending(p => p.ProductId).Take(takeSize);
         }
     }
 
