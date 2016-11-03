139b216b11a9ed2246e10a65115e8bfe10f41112
PaulHuang <paul780524@hotmail.com>
2016-11-04 01:27:11 +0800

    51 練習將 ClientLoginViewModel 特定欄位修改為用 Password 欄位方式輸出

---
+++ MVC5Course/Controllers/OrdersController.cs
@@ -0,0 +1,31 @@
+﻿using MVC5Course.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data;
+using System.Data.Entity;
+using System.Web.Mvc;
+
+namespace MVC5Course.Controllers
+{
+    public class OrdersController : Controller
+    {
+        private FabricsEntities db = new FabricsEntities();
+        // GET: Orders
+        public ActionResult Index(int ClientId)
+        {
+            var order = db.Order.Include(o => o.Client).Where(p => p.ClientId == ClientId);
+            return View(order.ToList());
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if(disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
